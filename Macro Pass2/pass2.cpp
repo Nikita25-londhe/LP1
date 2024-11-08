33029ef10eb5773f084629a32cf42846b5e2b43d
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2024-11-08 09:21:42 +0530

    Update pass2.cpp

--- Macro Pass2/pass2.cpp
+++ Macro Pass2/pass2.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include<vector>
-#include<sstream>
-#include<fstream>
-#include<tuple>
-#include<tuple>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
 using namespace std;
 class Macro
 {
@@ -12,111 +12,53 @@ class Macro
 	vector<pair<string, string>>KPDT;
 	vector<tuple<string, int, int, int, int>>MNT;
 	vector<vector<string>>PNTAB;
-	int count = 0;
+	ifstream a, b, c, d,fin;
 	ofstream fout;
 public:
 	Macro()
 	{
-		string line, word;
 		fout.open("expanded_code.txt");
-		if (fout.fail())
-		{
-			cout << "\nError in opening file";
-			exit(1);
-		}
-		ifstream fin;
+		a.open("MDT.txt");
+		b.open("PNTAB.txt");
+		c.open("KPDT.txt");
+		d.open("MNT.txt");
 		fin.open("input.txt");
-		if (fin.fail())
+		if (fin.fail() || a.fail() || b.fail() || c.fail() || d.fail())
 		{
 			cout << "\nError in opening file";
 			exit(1);
 		}
+		string line,word;
 		while (getline(fin, line))
 		{
 			stringstream ss(line);
-			vector<string>token;
+			vector<string>v1;
 			while (ss >> word)
 			{
-				token.push_back(word);
+				v1.push_back(word);
 			}
-			MyToken.push_back(token);
+			MyToken.push_back(v1);
 		}
+		readMDT();
+		readPNTAB();
+		readKPDT();
+		readMNT();
 	}
-	void displayToken()
+	void readMDT()
 	{
-		for (int i = 0; i < MyToken.size(); i++)
-		{
-			for (int j = 0; j < MyToken[i].size(); j++)
-			{
-				cout << MyToken[i][j] << " ";
-			}
-			cout << endl;
-		}
-	}
-	int deal(string token, int size)
-	{
-		if (find(PNTAB[size].begin(), PNTAB[size].end(), token) != PNTAB[size].end())
-		{
-			return (find(PNTAB[size].begin(), PNTAB[size].end(), token) - PNTAB[size].begin()) + 1;
-		}
-		return -1;
-	}
-	int storeDef(int i)
-	{
-		int kp = 0, pp = 0;
-		int kpdtp = KPDT.size();
-		int mdtp = MDT.size();
-		string name = MyToken[i][0];
-		vector<string>temppntab;
-		for (int j = 1; j < MyToken[i].size(); j++)
-		{
-			string curr = MyToken[i][j];
-			if (curr.find("=") != string::npos)
-			{
-				int index = curr.find("=");
-				kp++;
-				string keyword = curr.substr(0, index);
-				string value = curr.substr(index + 1, curr.length() - index - 2);
-				KPDT.push_back({ keyword,value });
-				temppntab.push_back(keyword);
-			}
-			else
-			{
-				temppntab.push_back(curr.substr(0, curr.length() - 1));
-				pp++;
-			}
-		}
-
-		MNT.emplace_back(name, pp, kp, mdtp, kpdtp);
-		PNTAB.push_back(temppntab);
-		int j;
-		for (j = i + 1; j < MyToken.size() && MyToken[j][0] != "MEND"; j++)
+		string line,word;
+		while (getline(a, line))
 		{
 			vector<string>v1;
-			v1.push_back(MyToken[j][0]);
-			for (int k = 1; k < MyToken[j].size(); k++)
+			stringstream ss(line);
+			while (ss >> word)
 			{
-				int index = deal(MyToken[j][k].substr(0, MyToken[j][k].length() - 1), PNTAB.size() - 1);
-				if (index == -1)
-				{
-					v1.push_back(MyToken[j][k]);
-				}
-				else
-				{
-					v1.push_back("(p," + to_string(index) + ")");
-				}
+				v1.push_back(word);
 			}
 			MDT.push_back(v1);
 		}
-		return j;
-	}
-	void displayMNT()
-	{
-		cout << "\nDisplaying MNT:";
-		for (tuple<string, int, int, int, int>t : MNT)
-		{
-			cout << "\n" << get<0>(t) << " " << get<1>(t) << " " << get<2>(t) << " " << get<3>(t) << " " << get<4>(t);
-		}
+		displayMDT();
+
 	}
 	void displayMDT()
 	{
@@ -130,6 +72,21 @@ public:
 			cout << endl;
 		}
 	}
+	void readPNTAB()
+	{
+		string line, word;
+		while (getline(b, line))
+		{
+			vector<string>v1;
+			stringstream ss(line);
+			while (ss >> word)
+			{
+				v1.push_back(word);
+			}
+			PNTAB.push_back(v1);
+		}
+		displayPNTAB();
+	}
 	void displayPNTAB()
 	{
 		cout << "\nDisplaying PNTAB:";
@@ -141,6 +98,19 @@ public:
 			}
 		}
 	}
+	void readKPDT()
+	{
+		string line;
+		while (getline(c, line))
+		{
+			stringstream ss(line);
+			string keyword, value;
+			ss >> keyword;
+			ss >> value;
+			KPDT.push_back({ keyword,value });
+		}
+		displayKPDT();
+	}
 	void displayKPDT()
 	{
 		cout << "\nDisplaying KPDT:";
@@ -149,31 +119,31 @@ public:
 			cout << "\n" << p.first << " " << p.second;
 		}
 	}
-	void Tokenize()
+	void readMNT()
 	{
-		for (int i = 0; i < MyToken.size(); i++)
+		string line, word;
+		while (getline(d, line))
 		{
-			if (MyToken[i][0] == "MACRO")
-			{
-				i = storeDef(i + 1);
-			}
-			count = i;
+			stringstream ss(line);
+			tuple<string, int, int, int, int>t1;
+			ss >> get<0>(t1) >> get<1>(t1) >> get<2>(t1) >> get<3>(t1) >> get<4>(t1);
+			MNT.push_back(t1);
 		}
+		displayMNT();
 	}
-	int getMNTIndex(string name)
+	void displayMNT()
 	{
-		for (int i = 0; i < MNT.size(); i++)
+		cout << "\nDisplaying MNT:";
+		for (tuple<string, int, int, int, int>t : MNT)
 		{
-			if (get<0>(MNT[i]) == name)
-			{
-				return i;
-			}
+			cout << "\n" << get<0>(t) << " " << get<1>(t) << " " << get<2>(t) << " " << get<3>(t) << " " << get<4>(t);
 		}
-		return -1;
 	}
 	void expandMacro(vector<string>token)
 	{
 		string macro_name = token[0];
+		cout << "\nExpanding macro " << macro_name;
+		fout<< "\nExpanding macro " << macro_name;
 		int index = getMNTIndex(macro_name);
 		if (index == -1)
 		{
@@ -183,21 +153,21 @@ public:
 		int pp = get<1>(MNT[index]);
 		int kp = get<2>(MNT[index]);
 		int MDTP = get<3>(MNT[index]);
-		int KPDTP= get<4>(MNT[index]);
+		int KPDTP = get<4>(MNT[index]);
 		vector<string>APTAB(pp + kp, "NULL");
 		int i;
 		for (i = 0; i < pp; i++)
 		{
-			APTAB[i] =token[i+1];
+			APTAB[i] = token[i + 1];
 		}
 		for (int j = i + 1; j < token.size(); j++)
 		{
 			if (token[j].find("=") != string::npos)
 			{
 				int ind = token[j].find("=");
-				string keyword = token[j].substr(0,ind);
-				string value =token[j].substr(ind+1);
-				int pntindex=getPNTABIndex(index,keyword);
+				string keyword = token[j].substr(0, ind);
+				string value = token[j].substr(ind + 1);
+				int pntindex = getPNTABIndex(index, keyword);
 				if (pntindex == -1)
 				{
 					cout << "\n" << keyword << " is not prsent in PNTAB";
@@ -220,10 +190,10 @@ public:
 				}
 			}
 		}
-		displayAPTAB(macro_name,APTAB);
+		displayAPTAB(macro_name, APTAB);
 		//Expanding MDT
 		int limit;
-		if(index+1<MNT.size())
+		if (index + 1 < MNT.size())
 		{
 			limit = get<3>(MNT[index + 1]);
 		}
@@ -231,15 +201,15 @@ public:
 		{
 			limit = MDT.size();
 		}
-		for (int i = MDTP; i <limit; i++)
+		for (int i = MDTP; i < limit; i++)
 		{
 			fout << endl;
 			for (int j = 0; j < MDT[i].size(); j++)
 			{
 				if (MDT[i][j].find("(") != string::npos)
 				{
-					int aptabind = stoi(MDT[i][j].substr(3, 1))-1;
-					fout << " "<<APTAB[aptabind];
+					int aptabind = stoi(MDT[i][j].substr(3, 1)) - 1;
+					fout << " " << APTAB[aptabind];
 				}
 				else
 				{
@@ -248,7 +218,18 @@ public:
 			}
 		}
 	}
-	int getPNTABIndex(int index,string key)
+	int getMNTIndex(string name)
+	{
+		for (int i = 0; i < MNT.size(); i++)
+		{
+			if (get<0>(MNT[i]) == name)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	int getPNTABIndex(int index, string key)
 	{
 		for (int i = 0; i < PNTAB[index].size(); i++)
 		{
@@ -259,7 +240,7 @@ public:
 		}
 		return -1;
 	}
-	void displayAPTAB(string macro,vector<string>APTAB)
+	void displayAPTAB(string macro, vector<string>APTAB)
 	{
 		cout << "\nDisplaying APTAB for :" << macro;
 		for (int i = 0; i < APTAB.size(); i++)
@@ -269,21 +250,16 @@ public:
 	}
 	void pass2()
 	{
-		for (int i = count-1; i < MyToken.size(); i++)
+		for (int i =0; i < MyToken.size(); i++)
 		{
+			cout << endl;
 			expandMacro(MyToken[i]);
+
 		}
 	}
 };
 int main()
 {
 	Macro m1;
-	m1.displayToken();
-	m1.Tokenize();
-	m1.displayMNT();
-	m1.displayPNTAB();
-	m1.displayKPDT();
-	m1.displayMDT();
 	m1.pass2();
-	return 0;
 }
