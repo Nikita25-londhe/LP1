5b93f4f39e20356cab73981d6db315f384634c22
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2024-11-08 21:04:58 +0530

    Update pass2.cpp

--- Micro pass2/pass2.cpp
+++ Micro pass2/pass2.cpp
@@ -1,113 +1,91 @@
-#include <iostream>
+#include<iostream>
 #include<vector>
-#include<fstream>
 #include<sstream>
+#include<fstream>
 using namespace std;
-class Pass_02 {
-    ifstream sy, li, fin;
-    ofstream fout;
-    vector<pair<string, int>> symtable;
-    vector<pair<string, int>> littable;
-
+class Pass2
+{
+	vector<pair<string, string>>SymTab;
+	ifstream sym, fin;
+	ofstream fout;
 public:
-    void input() {
-        sy.open("sym1.txt");
-        li.open("lit.txt");
-        fin.open("output.txt");
-        fout.open("machine1.txt");
-    }
+	Pass2()
+	{
+		fin.open("output.txt");
+		sym.open("sym1.txt");
+		fout.open("machine1.txt");
+		if (fin.fail() || sym.fail() || fout.fail())
+		{
+			cout << "\nError in opening file";
+			exit(1);
+		}
+		cout << "\nFiles opened successfully!!";
+		GetSymTab();
+	}
+	void GetSymTab()
+	{
+		string line,label,symbol,location;
+		while (getline(sym, line))
+		{
+			stringstream ss(line);
+			ss >> label >> symbol >> location;
+			SymTab.push_back({ symbol,location });
+		}
+	}
+	void genearate()
+	{
+		fout.seekp(0, ios::beg);
+		string line,location,opcode,word;
+		while (getline(fin, line))
+		{
+			fout << "\n";
+			stringstream ss(line);
+			ss >> location>>opcode;
+			if (opcode == "(IS,")
+			{
+				ss >> word;
+				fout << location << " " << word.substr(0, word.length() - 1);
+				while (ss >> word)
+				{
+					if (word == "(S,")
+					{
+						ss >> word;
+						int index = stoi(word.substr(0, word.length() - 1))-1;
+						fout << " " << SymTab[index].second;
 
-    void generate() {
-        string line = "", word = "";
-
-        // Load Symbol Table
-        while (getline(sy, line)) {
-            stringstream ss(line);
-            string label;
-            int address;
-            ss >> label >> word >> address; // Read the symbol number, symbol name (ignore), and address
-            symtable.push_back({ word, address });
-        }
-
-        // Load Literal Table
-        while (getline(li, line)) {
-            stringstream ss(line);
-            string label;
-            int address;
-            ss >> label >> address;
-            littable.push_back({ label, address });
-        }
-
-        // Generate Machine Code from Intermediate Code
-        while (getline(fin, line)) {
-            stringstream ss(line);
-            string location, opcode, operand;
-            ss >> location >> opcode; // Read the location and opcode
-
-            if (opcode == "(IS,") {
-                string code;
-                ss >> code;
-                string instruction = code.substr(0, code.size() - 1); // Remove the closing parenthesis
-                fout << location << " " << instruction << " "; // Write location and opcode to file
-
-                while (ss >> word) {
-                    if (word == "(S,") {
-                        ss >> operand;
-                        int symIndex = stoi(operand.substr(0, operand.size() - 1)) - 1;
-                        fout << symtable[symIndex].second << " "; // Write symbol address
-                    }
-                    else if (word == "(L,") {
-                        ss >> operand;
-                        int litIndex = stoi(operand.substr(0, operand.size() - 1)) - 1;
-                        fout << littable[litIndex].second << " "; // Write literal address
-                    }
-                    else if (word.find("(C,") != string::npos) {
-                        fout << word.substr(0, word.size() - 1) << " "; // Write constant
-                    }
-                    else {
-                        fout << word << " "; // Write register codes or anything else
-                    }
-                }
-            }
-            else if (opcode == "(DL,") {
-                string directive;
-                ss >> directive;
-                if (directive == "01)") { // DC
-                    ss >> word;
-                    ss >> word;
-                    fout << location << " 00 00 " << word.substr(0, word.size() - 1) << "\n"; // Write DC with constant
-                }
-                else if (directive == "02)") {
-                    fout << location << " 00 00 00\n";
-                }
-            }
-            fout << "\n"; // New line after each machine instruction
-        }
-    }
-
-    void show() {
-        cout << "\n===========Symbol table ==============" << endl;
-        for (auto x : symtable) {
-            cout << x.first << " " << x.second << "\n";
-        }
-        cout << "\n===========Literal table ===============" << endl;
-        for (auto x : littable) {
-            cout << x.first << " " << x.second << "\n";
-        }
-    }
-
-    ~Pass_02() {
-        sy.close();
-        li.close();
-        fin.close();
-        fout.close();
-    }
+					}
+					else if (word == "(C,")
+					{
+                        ss>>word;
+                        fout<<word.substr(0,word.length()-1);
+					}
+					else
+					{
+						fout << " "<<word;
+					}
+				}
+			}
+			else if (opcode == "(DL,")
+			{
+				ss >> word;
+				if (word == "02)")
+				{
+					fout << location << " 00 00 00 ";
+				}
+				else if (word == "01)")
+				{
+					fout << location << " 00 00 ";
+					ss >> word;
+					ss >> word;
+					fout << word.substr(0, word.length() - 1);
+				}
+			}
+		}
+	}
 };
-
-int main() {
-    Pass_02 obj;
-    obj.input();
-    obj.generate();
-    obj.show();
-    return 0;
+int main()
+{
+	Pass2 obj;
+	obj.genearate();
+	return 0;
 }
